f30e7138fea466a03573debff784c31219fe4e96
shahman-riaz <shahmanriaz07@gmail.com>
2025-05-15 15:18:18 +0600

    algorithm, time complexity, space complexity, big-o notation, time complexities, linear time complexites, fibonacci series - linear time complexity O(n)

--- index1.js
+++ index1.js
@@ -1,1 +1,21 @@
-// An algorithm is a set of well-defined instructions to solve a particular problem.
+// Fibonacci sequence
+// The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones, usually starting with 0 and 1.
+
+function fibonacci(n){
+    const fib = [0,1]
+
+    for(let i=2; i<n; i++){
+        fib[i] = fib[i-1] + fib[i-2]
+    }
+
+    return fib
+
+}
+
+
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(7));
+
+
+// Big-O = O(n) ; linear time complexity
